a8f7b429927af3a9431732887c71d56a8a5504c7
USER <samir_reaz@yahoo.com>
2019-11-27 00:27:11 +0600

    initial state 2

---
+++ app/src/main/java/com/rosetvonline/rosetvonline/splashScreen/SplashScreenFragment.java
@@ -0,0 +1,32 @@
+package com.rosetvonline.rosetvonline.splashScreen;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rosetvonline.rosetvonline.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SplashScreenFragment extends Fragment {
+
+
+    public SplashScreenFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false);
+    }
+
+}
